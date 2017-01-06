590847806d3c494d69f3a18923fbdce6896c2a97
jianqing <jianqing_sun@apple.com>
2017-01-06 15:49:43 -0800

    exercise 89

---
+++ src/main/java/com/jianqing/interview/leetcode16/GrayCode_89.java
@@ -0,0 +1,20 @@
+package com.jianqing.interview.leetcode16;
+
+import java.util.List;
+
+/**
+ * Created by jianqing_sun on 1/4/17.
+ */
+public class GrayCode_89 implements Solution {
+    @Override
+    public void solution() {
+
+    }
+    /*
+        多写几组能看出规律
+        n=k时的Gray Code，相当于n=k-1时的Gray Code的逆序 加上 1<<k
+     */
+    public List<Integer> grayCode(int n) {
+        return null;
+    }
+}
