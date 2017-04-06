13fa4bca5978e8751f8d81f3afec7a3d20380b5a
jianqing <jianqing_sun@apple.com>
2017-04-06 16:16:51 -0700

    120 Triangle

---
+++ src/main/java/com/jianqing/interview/leetcode/Triangle_120.java
@@ -0,0 +1,13 @@
+package com.jianqing.interview.leetcode;
+
+import java.util.List;
+
+/**
+ * Created by jianqing_sun on 4/6/17.
+ */
+public class Triangle_120 {
+
+    public int minimumTotal(List<List<Integer>> triangle) {
+
+    }
+}
