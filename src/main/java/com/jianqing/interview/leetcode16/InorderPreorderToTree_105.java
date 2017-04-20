3b3108b0821055e3dd5e616cb2ef4cc52e9b4124
Jianqing_Sun <sunjianqing@gmail.com>
2017-04-19 22:38:44 -0700

    more questions

---
+++ src/main/java/com/jianqing/interview/leetcode16/InorderPreorderToTree_105.java
@@ -0,0 +1,36 @@
+package com.jianqing.interview.leetcode16;
+
+import com.jianqing.interview.base.TreeNode;
+
+/**
+ * Created by jianqingsun on 4/17/17.
+ */
+public class InorderPreorderToTree_105 {
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        if (preorder.length == 0 || inorder.length == 0)
+            return null;
+
+        return helper(preorder, inorder, 0, preorder.length - 1, 0, inorder.length - 1);
+    }
+
+    private TreeNode helper(int[] preorder, int[] inorder, int preStart, int preEnd, int inStart, int inEnd) {
+
+        if (preEnd < preStart || inEnd < inStart)
+            return null;
+
+        int val = preorder[preStart];
+        TreeNode root = new TreeNode(val);
+        int index = 0;
+        for (int i = inStart; i <= inEnd; i++) {
+            if (inorder[i] == val) {
+                index = i;
+                break;
+            }
+        }
+
+        root.left = helper(preorder, inorder, preStart + 1, preStart + index - inStart, inStart, index - 1);
+        root.right = helper(preorder, inorder, preStart + index - inStart + 1, preEnd, index + 1, inEnd);
+
+        return root;
+    }
+}
--- src/main/java/com/jianqing/interview/leetcode16/InsertionSortList_147.java
+++ src/main/java/com/jianqing/interview/leetcode16/InsertionSortList_147.java
@@ -7,37 +7,41 @@ import com.jianqing.interview.leetcode.ListNode;
  */
 public class InsertionSortList_147 {
 
-    public ListNode insertionSortList(ListNode head) {
-
-        if (head == null || head.next == null) {
+    // 用这个算法
+    // 算法其实是把原list 劈成了两半， 一半是排过序的 （最后一个元素的next 是null）， 另一半是以current 指针为head
+    // 每次就把 current 元素加到已排好序的list 里， 这就跟insertion sort 的原理一样了
+    public ListNode insertionSortList_2nd(ListNode head) {
+        if (head == null || head.next == null)
             return head;
+        ListNode dummy = new ListNode(0);
+        // 注意 !!! 这里的dummy node 不用设定 dummy.head = head
+        ListNode cur = head;
+
+        //
+        while (cur != null) {
+            ListNode next = cur.next;
+            ListNode pre = dummy;
+            while (pre.next != null && pre.next.val < cur.val)
+                pre = pre.next;
+            cur.next = pre.next;
+            pre.next = cur;
+            cur = next;
         }
+        return dummy.next;
+    }
 
-        ListNode plast, pcurrent, pindex;
-        plast = head;
-        pcurrent = head.next;
-        pindex = new ListNode(0);
-        pindex = head ;
+    public static void main(String[] args) {
+        InsertionSortList_147 ins = new InsertionSortList_147();
+        ListNode head = new ListNode(5);
+        ListNode node1 = new ListNode(6);
+        head.next = node1;
+        ListNode node2 = new ListNode(10);
+        node1.next = node2;
+        ListNode node3 = new ListNode(12);
+        node2.next = node3;
+        ListNode node4 = new ListNode(8);
+        node3.next = node4;
 
-        int index = 0 ;
-
-
-        while (pcurrent != null) {
-            // find correct index
-            int i = 0;
-            while (i <= index){
-                if(pcurrent.val < pindex.val){
-                    plast.next = pcurrent.next;
-                }
-                else {
-                    pindex = pindex.next;
-                    i++;
-                }
-            }
-
-        }
-
-        return null;
-
+        ins.insertionSortList_2nd(head);
     }
 }
