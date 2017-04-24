2752d5947426ef90ac7af435b5b67774c189f209
jianqing <jianqing_sun@apple.com>
2017-04-23 17:47:20 -0700

    more

---
+++ src/main/java/com/jianqing/interview/simple/StringTest.java
@@ -0,0 +1,20 @@
+package com.jianqing.interview.simple;
+
+/**
+ * Created by jianqing on 11/11/15.
+ */
+public class StringTest {
+    public static void main(String[] args){
+        String s =  "hello world";
+        String s1 = s.substring(1,3);
+        String s2 = s.substring(2,5);
+        System.out.println(s1);
+        System.out.println(s2);
+
+        int i = 3,j = 4;
+        double v = ((double) i + j ) / 2;
+        System.out.println(v);
+    }
+
+
+}
