ba6f43fa5efca3e096f5475b72ef9f784b0a0e2a
jianqing_sun <jianqing_sun@apple.com>
2018-01-19 15:09:33 -0800

    more code

---
+++ src/main/java/com/jianqing/interview/basic/StreamDemo.java
@@ -0,0 +1,15 @@
+package com.jianqing.interview.basic;
+
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+/**
+ * Created by jianqing_sun on 1/17/18.
+ */
+
+public class StreamDemo {
+    public static void main(String[] args) {
+        Stream<Integer> stream = Arrays.asList(1, 2, 3, 4, 5).stream();
+
+    }
+}
