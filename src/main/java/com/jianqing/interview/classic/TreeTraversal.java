b0caacd05ad0a3f40af74c04655a434a01431c16
Jianqing_Sun <sunjianqing@gmail.com>
2018-03-15 23:18:56 -0700

    more code

--- src/main/java/com/jianqing/interview/classic/TreeTraversal.java
+++ src/main/java/com/jianqing/interview/classic/TreeTraversal.java
@@ -12,6 +12,21 @@ import java.util.Stack;
 public class TreeTraversal {
 
     public static void main(String[] args) {
+        TreeNode root = new TreeNode(10);
+
+        TreeNode n1 = new TreeNode(8);
+        TreeNode n2 = new TreeNode(11);
+        TreeNode n3 = new TreeNode(6);
+        TreeNode n4 = new TreeNode(9);
+
+        root.left = n1;
+        root.right = n2;
+        n1.left = n3;
+        n1.right = n4;
+
+        TreeTraversal tt = new TreeTraversal();
+        List list = tt.MorrisTraversal(root);
+        System.out.println(list);
 
     }
 
