ad52368ca46b2287e4c0556b2d20a0880f626f95
jianqing_sun <jianqing_sun@apple.com>
2018-03-20 13:30:57 -0700

    more code

---
+++ src/main/java/com/jianqing/interview/leetcode16/FriendCircles_547.java
@@ -0,0 +1,31 @@
+package com.jianqing.interview.leetcode16;
+
+/**
+ * Created by jianqing_sun on 3/20/18.
+ *
+ * DFS 一直找下去
+ */
+public class FriendCircles_547 {
+
+    public class Solution {
+        public void dfs(int[][] M, int[] visited, int i) {
+            for (int j = 0; j < M.length; j++) {
+                if (M[i][j] == 1 && visited[j] == 0) {
+                    visited[j] = 1;
+                    dfs(M, visited, j);
+                }
+            }
+        }
+        public int findCircleNum(int[][] M) {
+            int[] visited = new int[M.length];
+            int count = 0;
+            for (int i = 0; i < M.length; i++) {
+                if (visited[i] == 0) {
+                    dfs(M, visited, i);
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}
